5134e7212bda10aeea20112a1fe7fcd179b4eae4
shaaanuu <monshan50@gmail.com>
2025-01-02 15:44:06 +0530

    update: color scheme

--- lib/home.dart
+++ lib/home.dart
@@ -11,54 +11,54 @@ class Home extends StatelessWidget {
     ValueNotifier<String> valueNotifier = ValueNotifier(fact);
 
     return Scaffold(
-      backgroundColor: Colors.grey[900],
+      backgroundColor: Color(0xFFD5D8DC),
       appBar: AppBar(
-        elevation: 0,
+        elevation: 1,
         backgroundColor: Colors.transparent,
         centerTitle: true,
         title: const Text(
           'Facty',
-          style: TextStyle(fontSize: 20, color: Colors.white),
+          style: TextStyle(
+            fontSize: 20,
+            color: Color(0xFF2E4053),
+          ),
         ),
       ),
-      body: Scaffold(
-        backgroundColor: Colors.grey[900],
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ValueListenableBuilder(
-                valueListenable: valueNotifier,
-                builder: (context, String value, child) => Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    valueNotifier.value,
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                    ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ValueListenableBuilder(
+              valueListenable: valueNotifier,
+              builder: (context, String value, child) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  valueNotifier.value,
+                  style: const TextStyle(
+                    color: Color(0xFF2E4053),
+                    fontSize: 20,
                   ),
                 ),
               ),
-              const SizedBox(height: 30),
-              ElevatedButton(
-                onPressed: () async {
-                  final result = await getFact();
-                  valueNotifier.value = result.text.toString();
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.teal[300],
-                ),
-                child: const Text(
-                  'Click Me',
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () async {
+                final result = await getFact();
+                valueNotifier.value = result.text.toString();
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFFAAB7B8),
+              ),
+              child: const Text(
+                'Click Me',
+                style: TextStyle(
+                  color: Colors.black,
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
