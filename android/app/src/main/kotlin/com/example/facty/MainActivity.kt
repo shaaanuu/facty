e23390810addb030a7b33a73d56ba5086b8dc0fc
shaaanuu <monshan50@gmail.com>
2025-01-02 14:46:12 +0530

    rebuilt the project

---
+++ android/app/src/main/kotlin/com/example/facty/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.facty
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/random_facts/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.random_facts
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
